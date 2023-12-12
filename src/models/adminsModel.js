db16816bb97835876bd1130562eb92739fd672fa
fabiovitorino97 <fabiiovr@gmail.com>
2023-12-11 21:11:05 -0300

    feat: create adminsModel

---
+++ src/models/adminsModel.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs')
+
+const adminSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, "the field is required"]
+    },
+
+    email: {
+        type: String,
+        required: [true, "the field is required"],
+        unique: true,
+        match: [/^(([^<>()[]\.,;:\s@"]+(.[^<>()[]\.,;:\s@"]+)*)|(".+"))@(([[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}])|(([a-zA-Z-0-9]+.)+[a-zA-Z]{2,}))$/, 'Invalid email'],
+
+    },
+    password: {
+        type: String,
+        required: [true, "the field is required"],
+    },
+});
+
+adminSchema.pre('save', async function () {
+    const salt = await bcrypt.genSalt(10)
+    this.password = await bcrypt.hash(this.password, salt)
+})
+
+adminSchema.methods.comparePassword = async function (candidatePassword) {
+    const isMatch = await brypt.compare(candidatePassword, this.password)
+    return isMatch
+}
+
+module.exports = mongoose.model("Admin", adminSchema)
