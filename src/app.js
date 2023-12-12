ad23158b485782c946fd99fa34e1c3f7688f707c
Pedro Henrique Mota <motapedrofsp@gmail.com>
2023-12-11 21:30:19 -0300

    feat: Linked MongoDB

--- src/app.js
+++ src/app.js
@@ -1,5 +1,8 @@
 require("dotenv").config();
 require("express-async-errors");
+const mongoose = require("mongoose");
+
+
 const express = require("express");
 const app = express();
 
@@ -10,17 +13,21 @@ app.use(express.json());
 
 // routes
 app.get("/", (req, res) => {
-  res.send("Students Crud");
+  res.send("University Crud");
 });
+
+// Routes
+app.use('/api')
 
 
 const port = process.env.PORT || 3000;
 
 const start = async () => {
   try {
-    app.listen(port, () =>
-      console.log(`Server is listening on port ${port}...`)
-    );
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("Database connected!");
+
+    app.listen(port, () => console.log(`Server running on port ${port}...`));
   } catch (error) {
     console.log(error);
   }
