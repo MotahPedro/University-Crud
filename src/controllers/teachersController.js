1b0c47b129cd5c2dfc1143d364a75766de21e87d
Pedro Henrique Mota <motapedrofsp@gmail.com>
2023-12-13 20:06:15 -0300

    feat: created teachers login controller

--- src/controllers/teachersController.js
+++ src/controllers/teachersController.js
@@ -10,3 +10,33 @@ const register = async (req, res) => {
     teacher: { _id: teacher._id, name: teacher.name, role: "teacher" },
   });
 };
+
+const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    throw new CustomApiErrors.BadRequestError(
+      "Please provide email and password"
+    );
+  }
+
+  const teacher = await Teacher.findOne({ email });
+  if (!teacher) {
+    throw new CustomApiErrors.UnauthenticatedError("Invalid Credentials");
+  }
+
+  const isPasswordCorrect = await teacher.comparePassword(password);
+  if (!isPasswordCorrect) {
+    throw new CustomApiErrors.UnauthenticatedError("Invalid Credentials");
+  }
+
+  const token = createJwt(teacher, "teacher");
+  setResponseCookie(token, res);
+
+  return res
+    .status(StatusCodes.OK)
+    .json({
+      student: { _id: student._id, name: student.name, role: "student" },
+      msg: "Logged in successfully",
+    });
+};
