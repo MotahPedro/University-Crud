7d52033e1274731e7954c3cb52a50905fe797964
Pedro Henrique Mota <motapedrofsp@gmail.com>
2023-12-13 19:49:09 -0300

    feat: created admin login controller

--- src/controllers/adminsController.js
+++ src/controllers/adminsController.js
@@ -14,3 +14,33 @@ const register = async (req, res) => {
     admin: { _id: admin._id, name: admin.name, role: "admin" },
   });
 };
+
+const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    throw new CustomApiErrors.BadRequestError(
+      "Please provide email and password"
+    );
+  }
+
+  const admin = await Admin.findOne({ email });
+  if (!admin) {
+    throw new CustomApiErrors.UnauthenticatedError("Invalid Credentials");
+  }
+
+  const isPasswordCorrect = await admin.comparePassword(password);
+  if (!isPasswordCorrect) {
+    throw new CustomApiErrors.UnauthenticatedError("Invalid Credentials");
+  }
+
+  const token = createJwt(admin, "admin");
+  setResponseCookie(token, res);
+
+  return res
+    .status(StatusCodes.OK)
+    .json({
+      admin: { _id: admin._id, name: admin.name, role: "admin" },
+      msg: "Logged in successfully",
+    });
+};
