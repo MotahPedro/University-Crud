f30c9bfe859011db1ce7b8e5e3beca4a9c64d174
Pedro Henrique Mota <motapedrofsp@gmail.com>
2023-12-20 19:45:37 -0300

    fix: Fixed Email Regex
    Regex wasn't working propperly

--- src/models/teachersModel.js
+++ src/models/teachersModel.js
@@ -2,22 +2,24 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs')
 
 const teacherSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: [true, "the field is required"]
-    },
+  name: {
+    type: String,
+    required: [true, "the field is required"],
+  },
 
-    email: {
-        type: String,
-        required: [true, "the field is required"],
-        unique: true,
-        match: [/^(([^<>()[]\.,;:\s@"]+(.[^<>()[]\.,;:\s@"]+)*)|(".+"))@(([[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}])|(([a-zA-Z-0-9]+.)+[a-zA-Z]{2,}))$/, 'Invalid email'],
-
-    },
-    password: {
-        type: String,
-        required: [true, "the field is required"],
-    },
+  email: {
+    type: String,
+    required: [true, "the field is required"],
+    unique: true,
+    match: [
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+      "Invalid email",
+    ],
+  },
+  password: {
+    type: String,
+    required: [true, "the field is required"],
+  },
 });
 
 teacherSchema.pre('save', async function () {
